27408d20813aac367da4e60566b19d6393ef4958
Wild Kemijoki huskies Oy <rdd.bu4@gmail.com>
2025-05-21 14:15:32 +0200

    Fix knowBeforeYouGo text display

--- src/components/ExperienceDetail.tsx
+++ src/components/ExperienceDetail.tsx
@@ -254,7 +254,7 @@ export function ExperienceDetail({ experience, onClose, onBook }: ExperienceDeta
                     <div className="flex flex-col min-h-0 overflow-y-auto">
                       {experience.knowBeforeYouGo && (
                         <div
-                          className="text-gray-600 prose max-w-none"
+                          className="text-gray-600 max-w-full break-words
                           dangerouslySetInnerHTML={{ __html: experience.knowBeforeYouGo }}
                         />
                       )}
