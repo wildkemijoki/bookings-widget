b58a9978088d547f9b260e3a2369ddf2306476ee
Wild Kemijoki huskies Oy <rdd.bu4@gmail.com>
2025-05-21 07:21:48 +0200

    Updated OptionsStep.tsx

--- src/components/bookingprocess/OptionsStep.tsx
+++ src/components/bookingprocess/OptionsStep.tsx
@@ -38,6 +38,7 @@ export function OptionsStep({
   }
 
   // Check if transportation is available
+  console.log(timeSlot.pickupPlaces);
   const transportAvailable = timeSlot.pickupPlaces?.length > 0 && timeSlot.transportAvailable;
 
   return (
