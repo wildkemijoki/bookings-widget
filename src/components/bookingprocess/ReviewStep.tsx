3d4ba9cec82e18341e02c03a7526a87256ce4d93
Wild Kemijoki huskies Oy <rdd.bu4@gmail.com>
2025-06-07 12:58:17 +0200

    Updated ReviewStep.tsx

--- src/components/bookingprocess/ReviewStep.tsx
+++ src/components/bookingprocess/ReviewStep.tsx
@@ -161,6 +161,7 @@ export function ReviewStep({
              .setLocale('cs')
              .toLocaleString(DateTime.DATE_MED);
     console.log(newDt.toString());
+    
     return newDt;
   };    
 
